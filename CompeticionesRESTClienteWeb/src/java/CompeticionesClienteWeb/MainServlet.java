25e083848ca77904cf3cc2e9bd6859e3bff74173
Squid <gabh@protonmail.com>
2019-05-16 15:01:52 +0100

    last changes

--- CompeticionesRESTClienteWeb/src/java/CompeticionesClienteWeb/MainServlet.java
+++ CompeticionesRESTClienteWeb/src/java/CompeticionesClienteWeb/MainServlet.java
@@ -65,95 +65,99 @@ public class MainServlet extends HttpServlet {
                             .filter(c -> c.getName().equals("token_competiciones"))
                             .map(Cookie::getValue)
                             .findAny();
-            Competiciones cCompeticiones = cs.getCompeticiones(Competiciones.class, token.get());
-            ArrayList<Competicion> competiciones = cCompeticiones.getCompeticiones();
+            try{
+                Competiciones cCompeticiones = cs.getCompeticiones(Competiciones.class, token.get());
+                ArrayList<Competicion> competiciones = cCompeticiones.getCompeticiones();
 
-            out.println("<h3> Competiciones:</h3>");
-            out.println("<div>");
-            if (competiciones.isEmpty())
-                out.println("<h4>No hay competiciones</h4>");
-            else
-                for(Competicion competicion : competiciones){
-                    out.println("<div style='display: inline-block'>");
-                        out.println("<h4>" + competicion.getId() + " - " + competicion.getNombre() + "</h4>");
-                        out.println("<h5>Deportes </h5>");
-                        out.println("<ul>");
-                        for (Deporte deporte : competicion.getDeportes()){
-                            out.println("<li>");
-                                out.println("<ul>");
-                                    out.println("<li><b>ID:</b> " + deporte.getId() + "</li>");
-                                    out.println("<li><b>Nombre:</b> " + deporte.getNombre() + "</li>");
-                                    out.println("<li><b>Tipo:</b> " + deporte.getTipo() + "</li>");
-                                    out.println("<li><b>Equipos:</b> " + deporte.getEquipos() + "</li>");
-                                    out.println("<li><b>Tamaño equipo:</b> " + deporte.getTamanoEquipo() + "</li>");
-                                out.println("</ul>");
-                            out.println("</li>");
-                        }
-                        out.println("</ul>");
-                    out.println("</div>");
-                }
-            out.println("</div>");
+                out.println("<h3> Competiciones:</h3>");
+                out.println("<div>");
+                if (competiciones.isEmpty())
+                    out.println("<h4>No hay competiciones</h4>");
+                else
+                    for(Competicion competicion : competiciones){
+                        out.println("<div style='display: inline-block'>");
+                            out.println("<h4>" + competicion.getId() + " - " + competicion.getNombre() + "</h4>");
+                            out.println("<h5>Deportes </h5>");
+                            out.println("<ul>");
+                            for (Deporte deporte : competicion.getDeportes()){
+                                out.println("<li>");
+                                    out.println("<ul>");
+                                        out.println("<li><b>ID:</b> " + deporte.getId() + "</li>");
+                                        out.println("<li><b>Nombre:</b> " + deporte.getNombre() + "</li>");
+                                        out.println("<li><b>Tipo:</b> " + deporte.getTipo() + "</li>");
+                                        out.println("<li><b>Equipos:</b> " + deporte.getEquipos() + "</li>");
+                                        out.println("<li><b>Tamaño equipo:</b> " + deporte.getTamanoEquipo() + "</li>");
+                                    out.println("</ul>");
+                                out.println("</li>");
+                            }
+                            out.println("</ul>");
+                        out.println("</div>");
+                    }
+                out.println("</div>");
 
-            
-//            http://localhost:8080/CompeticionesRESTClass/CompeticionesService?tester
-//            out.println(""
-//                    + "<script type=\"text/javascript\">\n" +
-//                        "function OnSubmitForm(){\n" +
-//                        "  var id = document.anadirDeporte.competicion;" +
-//                        "  document.anadirDeporte.action ='competicion/' + id;\n" +
-//                        "  return true;\n" +
-//                        "}\n" +
-//                        "function obtenerDeporte(){\n" + 
-//                        "  var idCompeticion = document.anadirDeporte.competicion;" +
-//                        "  var idDeporte = document.anadirDeporte.deporte;" +
-//                        "  document.anadirDeporte.action ='competicion/' + idCompeticion + '/deporte/' + idDeporte;\n" +
-//                        "  return true;\n" +
-//                        "}\n" +
-//                        "</script>");
-            out.println("<h3>Compartir competición </h3>");
-            out.println("<form action='/CompeticionesRESTClienteWeb/compartir' method = \"POST\">");
-                out.println("<div>ID Competicion: <input type = \"text\" name = 'competicion' /></div>");
-                out.println("<div>Nombre usuario: <input type = \"text\" name = 'username' /></div>");
-                out.println("<input type = \"submit\" value = \"Mandar\" />");
-            out.println("</form>");
-            out.println("<h3>Añadir deporte</h3>");
-            out.println("<form action='/CompeticionesRESTClienteWeb/deportes' method = \"POST\">");
-                out.println("<div>ID Competicion: <input type = \"text\" name = 'competicion' /></div>");
-                out.println("<div>Nombre: <input type = \"text\" name = 'nombre' /></div>");
-                out.println("<div>Tipo: <input type = \"text\" name = \"tipo\" /></div>");
-                out.println("<div>Equipos: <input type = \"text\" name = \"equipos\" /></div>");
-                out.println("<div>Tamaño equipo: <input type = \"text\" name = \"tamanoEquipo\" /></div>");
-                out.println("<input type = \"submit\" value = \"Mandar\" />");
-            out.println("</form>");
-            out.println("<h3>Obtener deporte</h3>");
-            out.println("<form action = '/CompeticionesRESTClienteWeb/deportes' method = \"GET\">");
-                out.println("ID competicion: <input type = \"text\" name = \"competicion\" />");
-                out.println("ID deporte: <input type = \"text\" name = 'deporte' />");
-                out.println("<input type = \"submit\" value = \"Mandar\" />");
-            out.println("</form>");
-            out.println("<h3>Borrar deporte</h3>");
-            out.println("<form action = '/CompeticionesRESTClienteWeb/delete' method = \"POST\">");
-                out.println("ID competicion: <input type = \"text\" name = \"competicion\" />");
-                out.println("ID deporte: <input type = \"text\" name = 'deporte' />");
-                out.println("<input type = \"submit\" value = \"Mandar\" />");
-            out.println("</form>");
-            out.println("<h3>Añadir competicion</h3>");
-            out.println("<form action='/CompeticionesRESTClienteWeb/competiciones' method = \"POST\">");
-                out.println("<div>Nombre: <input type = \"text\" name = 'nombre' /></div>");
-                out.println("<input type = \"submit\" value = \"Mandar\" />");
-            out.println("</form>");
-            out.println("<h3>Borrar competicion</h3>");
-            out.println("<form action = '/CompeticionesRESTClienteWeb/delete' method = \"POST\">");
-                out.println("ID competicion: <input type = \"text\" name = \"competicion\" />");
-                out.println("<input type = \"submit\" value = \"Mandar\" />");
-            out.println("</form>");
-            out.println("<h3>Validar archivo</h3>");
-            out.println(" <form action=\"validarArchivo\" enctype='multipart/form-data' method='POST'>\n" +
-                "  <input type=\"file\" name=\"archivo\" accept='.xml'>\n" +
-                "  <input type=\"submit\" value='Mandar'>\n" +
-                "</form> ");
-            out.println("</body>");
-            out.println("</html>");
+
+    //            http://localhost:8080/CompeticionesRESTClass/CompeticionesService?tester
+    //            out.println(""
+    //                    + "<script type=\"text/javascript\">\n" +
+    //                        "function OnSubmitForm(){\n" +
+    //                        "  var id = document.anadirDeporte.competicion;" +
+    //                        "  document.anadirDeporte.action ='competicion/' + id;\n" +
+    //                        "  return true;\n" +
+    //                        "}\n" +
+    //                        "function obtenerDeporte(){\n" + 
+    //                        "  var idCompeticion = document.anadirDeporte.competicion;" +
+    //                        "  var idDeporte = document.anadirDeporte.deporte;" +
+    //                        "  document.anadirDeporte.action ='competicion/' + idCompeticion + '/deporte/' + idDeporte;\n" +
+    //                        "  return true;\n" +
+    //                        "}\n" +
+    //                        "</script>");
+                out.println("<h3>Compartir competición </h3>");
+                out.println("<form action='/CompeticionesRESTClienteWeb/compartir' method = \"POST\">");
+                    out.println("<div>ID Competicion: <input type = \"text\" name = 'competicion' /></div>");
+                    out.println("<div>Nombre usuario: <input type = \"text\" name = 'username' /></div>");
+                    out.println("<input type = \"submit\" value = \"Mandar\" />");
+                out.println("</form>");
+                out.println("<h3>Añadir deporte</h3>");
+                out.println("<form action='/CompeticionesRESTClienteWeb/deportes' method = \"POST\">");
+                    out.println("<div>ID Competicion: <input type = \"text\" name = 'competicion' /></div>");
+                    out.println("<div>Nombre: <input type = \"text\" name = 'nombre' /></div>");
+                    out.println("<div>Tipo: <input type = \"text\" name = \"tipo\" /></div>");
+                    out.println("<div>Equipos: <input type = \"text\" name = \"equipos\" /></div>");
+                    out.println("<div>Tamaño equipo: <input type = \"text\" name = \"tamanoEquipo\" /></div>");
+                    out.println("<input type = \"submit\" value = \"Mandar\" />");
+                out.println("</form>");
+                out.println("<h3>Obtener deporte</h3>");
+                out.println("<form action = '/CompeticionesRESTClienteWeb/deportes' method = \"GET\">");
+                    out.println("ID competicion: <input type = \"text\" name = \"competicion\" />");
+                    out.println("ID deporte: <input type = \"text\" name = 'deporte' />");
+                    out.println("<input type = \"submit\" value = \"Mandar\" />");
+                out.println("</form>");
+                out.println("<h3>Borrar deporte</h3>");
+                out.println("<form action = '/CompeticionesRESTClienteWeb/delete' method = \"POST\">");
+                    out.println("ID competicion: <input type = \"text\" name = \"competicion\" />");
+                    out.println("ID deporte: <input type = \"text\" name = 'deporte' />");
+                    out.println("<input type = \"submit\" value = \"Mandar\" />");
+                out.println("</form>");
+                out.println("<h3>Añadir competicion</h3>");
+                out.println("<form action='/CompeticionesRESTClienteWeb/competiciones' method = \"POST\">");
+                    out.println("<div>Nombre: <input type = \"text\" name = 'nombre' /></div>");
+                    out.println("<input type = \"submit\" value = \"Mandar\" />");
+                out.println("</form>");
+                out.println("<h3>Borrar competicion</h3>");
+                out.println("<form action = '/CompeticionesRESTClienteWeb/delete' method = \"POST\">");
+                    out.println("ID competicion: <input type = \"text\" name = \"competicion\" />");
+                    out.println("<input type = \"submit\" value = \"Mandar\" />");
+                out.println("</form>");
+                out.println("<h3>Validar archivo</h3>");
+                out.println(" <form action=\"validarArchivo\" enctype='multipart/form-data' method='POST'>\n" +
+                    "  <input type=\"file\" name=\"archivo\" accept='.xml'>\n" +
+                    "  <input type=\"submit\" value='Mandar'>\n" +
+                    "</form> ");
+                out.println("</body>");
+                out.println("</html>");
+            }catch (Exception e){
+                response.sendRedirect("/CompeticionesRESTClienteWeb/auth");
+            }
         }
         
         
